0d1613f04f75cffa5c22bda794b2efe446e41520
Amit <amiitp@gmail.com>
2016-12-30 10:09:44 +0530

    Initial Commit

---
+++ thapagitt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thapagitt
+//
+//  Created by Amit on 30/12/16.
+//  Copyright © 2016 sdfds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
